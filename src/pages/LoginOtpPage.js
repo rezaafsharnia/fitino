b8a1918933c1e17b4c8f7888ca877c12ded89dd2
Reza Afsharnia <s.reza.afsharnia@gmail.com>
2023-03-14 16:01:39 +0330

    added

--- src/pages/LoginOtpPage.js
+++ src/pages/LoginOtpPage.js
@@ -1,7 +1,11 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import FornConfirm from "../components/FornConfirm";
 
 function LoginOtpPage() {
+  useEffect(() => {
+    axios.get("http://localhost:5000/sendOtp");
+  });
   return (
     <div className="flex justify-center items-center">
       <FornConfirm />
